d72d515b8aa9ddb6b97cfcb257da4df1a3cea1fc
jerssych <yerchlib4@gmail.com>
2026-05-11 10:13:49 -0500

    Add files via upload

---
+++ Concatenacion.java
@@ -0,0 +1,9 @@
+class Concatenacion {
+    public static void main(String[] args) {
+        String nombre = "Jerssy";
+        String primerApellido = "Cuycapusa";
+        String nombreConApellido = nombre + " " + primerApellido;
+
+        System.out.println (nombreConApellido);
+    } // Fin del metodo main
+} // Fin de la clase Concatenacion
